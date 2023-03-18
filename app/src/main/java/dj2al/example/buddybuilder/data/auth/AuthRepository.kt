9826a933cf825faf0fc135451341cf8cebe50b06
JeanTollemer <118350556+JeanTollemer@users.noreply.github.com>
2023-03-18 08:41:34 +0100

    Create AuthRepository.kt

---
+++ app/src/main/java/dj2al/example/buddybuilder/data/auth/AuthRepository.kt
@@ -0,0 +1,11 @@
+package app.dj2al.example.buddybuilder.data.auth
+
+import app.dj2al.example.buddybuilder.data.Resource
+import com.google.firebase.auth.FirebaseUser
+
+interface AuthRepository {
+    val currentUser: FirebaseUser?
+    suspend fun login(email: String, password: String): Resource<FirebaseUser>
+    suspend fun signup(name: String, email: String, password: String): Resource<FirebaseUser>
+    fun logout()
+}
