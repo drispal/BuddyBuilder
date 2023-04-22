6a1168c92e6ac7260ab2351203f1a748f0ee8a39
Damien-Joseph <74458949+drispal@users.noreply.github.com>
2023-04-21 23:09:11 -0400

    User screen working

--- app/src/main/java/dj2al/example/buddybuilder/ui/home/user/UserScreen.kt
+++ app/src/main/java/dj2al/example/buddybuilder/ui/home/user/UserScreen.kt
@@ -2,14 +2,27 @@ package dj2al.example.buddybuilder.ui.home.user
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -17,6 +30,7 @@ import dj2al.example.buddybuilder.R
 import dj2al.example.buddybuilder.data.Resource
 import dj2al.example.buddybuilder.data.models.User
 import dj2al.example.buddybuilder.ui.commons.FullScreenProgressbar
+import dj2al.example.buddybuilder.ui.theme.BuddyBuilderTheme
 
 
 @Composable
@@ -38,6 +52,7 @@ fun UserScreen(viewModel: UserViewModel) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserData(user : User)
 {
@@ -49,24 +64,41 @@ fun UserData(user : User)
             modifier = Modifier
                 .fillMaxSize()
                 .padding(all = 16.dp)){
-            Image(painter = painterResource(id = R.drawable.ic_account_box),
-                contentDescription = "",
-                modifier = Modifier.size(200.dp))
+            Box(){
+                // Setting Icon
+                Image(painter = painterResource(id = R.drawable.ic_settings),
+                    colorFilter = ColorFilter.tint(Color.Gray),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .clickable { /*TODO*/ }
+                        .align(Alignment.BottomEnd)
+                        .padding(7.dp)
+                        .size(30.dp))
+                // User Picture
+                Image(painter = painterResource(id = R.drawable.ic_account_box),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .clip(shape = MaterialTheme.shapes.large))
+            }
             Spacer(modifier = Modifier.size(30.dp))
-            Text(text = "Mail : ${user.mail}",
-                fontSize = 30.sp)
+            Text(text = "Mike Ross", fontFamily = FontFamily(Font(R.font.oswald_variablefont_wght)), fontSize = 30.sp, fontWeight = FontWeight.W500, textAlign = TextAlign.Center)
             Spacer(modifier = Modifier.size(30.dp))
-            Text(text = "Localisation : ${user.location}",
-                fontSize = 20.sp)
+            Text(text = "mail : ${user.mail}", fontFamily = FontFamily(Font(R.font.oswald_variablefont_wght)), fontSize = 25.sp, fontWeight = FontWeight.W400, textAlign = TextAlign.Center)
             Spacer(modifier = Modifier.size(30.dp))
-            Text(text = "Distance acceptable du lieu d'activité : ${user.maxDistance} km",
-                fontSize = 15.sp)
+            Text(text = stringResource(id = R.string.localization) + " : ${user.location}", fontFamily = FontFamily(Font(R.font.oswald_variablefont_wght)), fontSize = 25.sp, fontWeight = FontWeight.W400, textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.size(15.dp))
+            Text(text = stringResource(id = R.string.distance_user) + " : ${user.maxDistance} km", fontFamily = FontFamily(Font(R.font.oswald_variablefont_wght)), fontSize = 25.sp, fontWeight = FontWeight.W400, textAlign = TextAlign.Center)
             Spacer(modifier = Modifier.size(30.dp))
-            Text(text = "Sports inscrits : ${user.subscribedSports}",
-                fontSize = 20.sp)
-            Spacer(modifier = Modifier.size(30.dp))
-            Text(text = "Evénements enregistrées (${user.subscribedEvents.size}): ${user.subscribedEvents}",
-                fontSize = 20.sp)
+            Text(text = "BADGES", fontFamily = FontFamily(Font(R.font.oswald_variablefont_wght)), fontSize = 25.sp, fontWeight = FontWeight.W400, textAlign = TextAlign.Center)
         }
+    }
+}
+
+@Composable
+@Preview
+fun UserScreenPreview() {
+    BuddyBuilderTheme() {
+        UserData(User("mike.ross@gmail.com", "Paris", 10, mutableListOf(), mutableListOf()))
     }
 }
